1737430487a063f1e584e59116403930b93fd03e
alexei <dorofeev86@yahoo.com>
2020-12-24 22:18:53 +0100

    created two routes

---
+++ src/pages/create-account.tsx
@@ -0,0 +1,3 @@
+import React from "react";
+
+export const CreateAccount = () => <div>Create Account</div>;
--- src/routers/logged-out-router.tsx
+++ src/routers/logged-out-router.tsx
@@ -1,41 +1,19 @@
 import React from "react";
-import { useForm } from "react-hook-form";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Login } from "../pages/login";
+import { CreateAccount } from "../pages/create-account";
 
 export const LoggedOutRouter = () => {
-  const { register, watch, handleSubmit, errors } = useForm();
-  const onSubmit = () => {
-    console.log(watch());
-  };
-  const onInvalid = () => {
-    console.log("cant create account");
-  };
   return (
-    <div>
-      <h1>Logged Out</h1>
-      <form onSubmit={handleSubmit(onSubmit, onInvalid)}>
-        <div>
-          <input
-            ref={register({
-              required: true,
-              validate: (email: string) => email.includes("@gmail.com"),
-            })}
-            type="email"
-            name="email"
-            placeholder="Email"
-          />
-        </div>
-        <div>
-          <input
-            ref={register({
-              required: true,
-            })}
-            type="password"
-            name="password"
-            placeholder="Placeholder"
-          />
-        </div>
-        <button className="bg-yellow-300 text-white">Submit</button>
-      </form>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/create-account">
+          <CreateAccount />
+        </Route>
+        <Route path="/">
+          <Login />
+        </Route>
+      </Switch>
+    </Router>
   );
 };
