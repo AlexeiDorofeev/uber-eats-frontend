619f201e8584a14fdd3f9a10a6f77c6d206be54f
alexei <dorofeev86@yahoo.com>
2020-12-25 11:50:17 +0100

    cloning ui

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -6,6 +6,7 @@ import {
   loginMutation,
   loginMutationVariables,
 } from "../__generated__/loginMutation";
+import uberLogo from "../images/1.svg";
 
 const LOGIN_MUTATION = gql`
   mutation loginMutation($loginInput: Logininput!) {
@@ -23,7 +24,13 @@ interface ILoginForm {
 }
 
 export const Login = () => {
-  const { register, getValues, errors, handleSubmit } = useForm<ILoginForm>();
+  const {
+    register,
+    getValues,
+    errors,
+    handleSubmit,
+    formState,
+  } = useForm<ILoginForm>();
 
   const onCompleted = (data: loginMutation) => {
     const {
@@ -34,25 +41,30 @@ export const Login = () => {
     }
   };
 
-  const [loginMutation, { data: loginMutationResult }] = useMutation<
+  const [loginMutation, { data: loginMutationResult, loading }] = useMutation<
     loginMutation,
     loginMutationVariables
   >(LOGIN_MUTATION, {
     onCompleted,
   });
   const onSubmit = () => {
-    const { email, password } = getValues();
-    loginMutation({
-      variables: { loginInput: { email, password } },
-    });
+    if (!loading) {
+      const { email, password } = getValues();
+      loginMutation({
+        variables: { loginInput: { email, password } },
+      });
+    }
   };
   return (
-    <div className="h-screen flex items-center justify-center bg-gray-800">
-      <div className="bg-white w-full max-w-lg pt-10 pb-7 rounded-lg text-center">
-        <h3 className="text-2xl text-gray-800">Log in</h3>
+    <div className="h-screen flex items-center flex-col mt-10 lg:mt-28">
+      <div className="w-full max-w-screen-sm flex flex-col px-5 items-center">
+        <img src={uberLogo} className="w-52 mb-5" />
+        <h4 className="w-full font-medium text-left text-3xl mb-10">
+          Welcome back
+        </h4>
         <form
           onSubmit={handleSubmit(onSubmit)}
-          className="grid gap-3 mt-5 px-5"
+          className="grid gap-3 mt-5 w-full"
         >
           <input
             ref={register({ required: "Email is required" })}
@@ -60,7 +72,7 @@ export const Login = () => {
             required
             type="email"
             placeholder="Email"
-            className="input mb-3"
+            className="input"
           />
           {errors.email?.message && (
             <FormError errorMessage={errors.email.message} />
@@ -79,7 +91,9 @@ export const Login = () => {
           {errors.password?.type === "minLength" && (
             <FormError errorMessage=" Password must be more than 10 charachters" />
           )}
-          <button className="btn mt-3">Login</button>
+          <button className={`btn ${!formState.isValid ? "bg-gray-300" : ""}`}>
+            {loading ? "Loading..." : "Login"}
+          </button>
           {loginMutationResult?.login.error && (
             <FormError errorMessage={loginMutationResult.login.error} />
           )}
