041169788e410078b1c20fccad023e102befb3a4
alexei <dorofeev86@yahoo.com>
2020-12-25 17:20:35 +0100

    create account mutation

--- src/pages/create-account.tsx
+++ src/pages/create-account.tsx
@@ -1,11 +1,16 @@
 import { gql, useMutation } from "@apollo/client";
+import { create } from "domain";
 import React from "react";
 import Helmet from "react-helmet";
 import { useForm } from "react-hook-form";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import { Button } from "../components/button";
 import { FormError } from "../components/form-error";
 import uberLogo from "../images/1.svg";
+import {
+  createAccountMutation,
+  createAccountMutationVariables,
+} from "../__generated__/createAccountMutation";
 import { UserRole } from "../__generated__/globalTypes";
 
 const CREATE_ACCOUNT_MUTATION = gql`
@@ -36,9 +41,32 @@ export const CreateAccount = () => {
     defaultValues: { role: UserRole.Client },
   });
 
-  const [createAccountMutation] = useMutation(CREATE_ACCOUNT_MUTATION);
-  const onSubmit = () => {};
-  console.log(watch());
+  const history = useHistory();
+  const onCompleted = (data: createAccountMutation) => {
+    const {
+      createAccount: { ok, error },
+    } = data;
+    if (ok) {
+      history.push("/login");
+    }
+  };
+  const [
+    createAccountMutation,
+    { loading, data: createAccountMutationResult },
+  ] = useMutation<createAccountMutation, createAccountMutationVariables>(
+    CREATE_ACCOUNT_MUTATION,
+    { onCompleted }
+  );
+
+  const onSubmit = () => {
+    if (!loading) {
+      const { email, password, role } = getValues();
+      createAccountMutation({
+        variables: { createAccountInput: { email, password, role } },
+      });
+    }
+  };
+
   return (
     <div className="h-screen flex items-center flex-col mt-10 lg:mt-28">
       <Helmet>
@@ -54,7 +82,10 @@ export const CreateAccount = () => {
           className="grid gap-3 mt-5 w-full mb-5"
         >
           <input
-            ref={register({ required: "Email is required" })}
+            ref={register({
+              required: "Email is required",
+              pattern: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+            })}
             name="email"
             required
             type="email"
@@ -63,6 +94,9 @@ export const CreateAccount = () => {
           />
           {errors.email?.message && (
             <FormError errorMessage={errors.email.message} />
+          )}
+          {errors.email?.type === "pattern" && (
+            <FormError errorMessage={"Please enter a valid email"} />
           )}
           <input
             ref={register({ required: "Password is required" })}
@@ -89,9 +123,14 @@ export const CreateAccount = () => {
           )}
           <Button
             canClick={formState.isValid}
-            loading={false}
+            loading={loading}
             actionText={"Create Account"}
           />
+          {createAccountMutationResult?.createAccount.error && (
+            <FormError
+              errorMessage={createAccountMutationResult.createAccount.error}
+            />
+          )}
         </form>
         <div>
           Already have an account?{" "}
