11cb725776bba80a9fd9fb01a8e3eeb78a0b47a7
alexei <dorofeev86@yahoo.com>
2020-12-25 21:16:33 +0100

    finished authentication

--- src/apollo.ts
+++ src/apollo.ts
@@ -1,13 +1,32 @@
-import { ApolloClient, InMemoryCache, makeVar } from "@apollo/client";
+import {
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache,
+  makeVar,
+} from "@apollo/client";
 import { LOCALSTORAGE_TOKEN } from "./constants";
+import { setContext } from "@apollo/client/link/context";
 
 const token = localStorage.getItem(LOCALSTORAGE_TOKEN);
 
 export const isLoggedInVar = makeVar(Boolean(token));
 export const authToken = makeVar(token);
 
-export const client = new ApolloClient({
+const httpLink = createHttpLink({
   uri: "http://localhost:4000/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+  return {
+    headers: {
+      ...headers,
+      "x-jwt": authToken() || "",
+    },
+  };
+});
+
+export const client = new ApolloClient({
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache({
     typePolicies: {
       Query: {
