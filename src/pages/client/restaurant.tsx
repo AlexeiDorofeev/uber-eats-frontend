5025cf69be94f20b631d5bd944756c9746d6c695
alexei <dorofeev86@yahoo.com>
2020-12-28 16:32:12 +0100

    restaurant part 2

--- src/pages/client/restaurant.tsx
+++ src/pages/client/restaurant.tsx
@@ -37,5 +37,24 @@ export const Restaurant = () => {
     }
   );
   console.log(data);
-  return <h1>Restaurant</h1>;
+  return (
+    <div>
+      <div
+        className="bg-gray-800 py-48 bg-center bg-cover"
+        style={{
+          backgroundImage: `url(${data?.restaurant.restaurant?.coverImg})`,
+        }}
+      >
+        <div className="bg-white w-3/12 py-8 pl-48">
+          <h4 className="text-4xl mb-3">{data?.restaurant.restaurant?.name}</h4>
+          <h5 className="text-sm font-light mb-2">
+            {data?.restaurant.restaurant?.category?.name}
+          </h5>
+          <h6 className="text-sm font-light">
+            {data?.restaurant.restaurant?.address}
+          </h6>
+        </div>
+      </div>
+    </div>
+  );
 };
