ae015d9353e9efc0e43e948e9770a5d36cec6012
alexei <dorofeev86@yahoo.com>
2020-12-28 23:03:46 +0100

    login test part 1

---
+++ src/pages/__test__/login.spec.tsx
@@ -0,0 +1,43 @@
+import { ApolloProvider } from "@apollo/client";
+import { render, RenderResult, waitFor } from "@testing-library/react";
+import { createMockClient } from "mock-apollo-client";
+import React from "react";
+import { HelmetProvider } from "react-helmet-async";
+import { Login } from "../login";
+import { BrowserRouter as Router } from "react-router-dom";
+import userEvent from "@testing-library/user-event";
+
+describe("<Login />", () => {
+  let renderResult: RenderResult;
+  beforeEach(async () => {
+    await waitFor(() => {
+      const mockedClient = createMockClient();
+      renderResult = render(
+        <HelmetProvider>
+          <Router>
+            <ApolloProvider client={mockedClient}>
+              <Login />
+            </ApolloProvider>
+          </Router>
+        </HelmetProvider>
+      );
+    });
+  });
+  it("should render OK", async () => {
+    await waitFor(() => {
+      expect(document.title).toBe("Login | Uber Eats");
+    });
+  });
+  it("displays email validation errors", async () => {
+    const { getByPlaceholderText, debug, getByRole } = renderResult;
+    const email = getByPlaceholderText(/email/i);
+    await waitFor(() => {
+      userEvent.type(email, "this@wont");
+    });
+    let errorMessage = getByRole("alert");
+    expect(errorMessage).toHaveTextContent(/please enter a valid email/i);
+    await waitFor(() => {
+      userEvent.clear(email);
+    });
+  });
+});
