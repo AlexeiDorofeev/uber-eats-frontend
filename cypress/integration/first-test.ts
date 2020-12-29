100c257fb2c4406c76c1e9097d2ae065591a730d
alexei <dorofeev86@yahoo.com>
2020-12-29 17:24:42 +0100

    login e2e test

--- cypress/integration/first-test.ts
+++ cypress/integration/first-test.ts
@@ -1,5 +1,16 @@
-describe("first Test", () => {
-  it("should go to homepage", () => {
-    cy.visit("http://localhost:3000").title().should("eq", "Login | Uber Eats");
+describe("Login", () => {
+  it("should see login page", () => {
+    cy.visit("/").title().should("eq", "Login | Uber Eats");
+  });
+  it("can fill out the form", () => {
+    cy.visit("/");
+    cy.findByPlaceholderText(/email/i).type("dorofeev86@yahoo.com");
+    cy.findByPlaceholderText(/password/i).type("12345");
+    cy.findByRole("button").should("not.have.class", "pointer-events-none");
+  });
+  it("can see email/password validation errors", () => {
+    cy.visit("/");
+    cy.findByPlaceholderText(/email/i).type("bad@email");
+    cy.findByRole("alert").should("have.text", "Please enter a valid email");
   });
 });
